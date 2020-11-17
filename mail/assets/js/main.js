48884adf434a25e58af7ae7e788ca13d98e79bbe
matteo <matteoezio@me.com>
2020-11-17 17:54:53 +0100

    esercizio completo

--- mail/assets/js/main.js
+++ mail/assets/js/main.js
@@ -4,10 +4,22 @@
 var userMail = prompt("Salve, qual è la tua Email?");
 console.log(userMail);
 
-// check email list
-if (userMail = ) {
+// emailCheckList
+var emaliCheckList = ["esercizio@boolean.it", "esercizio@boolean.com", "esercizio@boolean.net"]
 
+// verificatore di accesso
+if (userMail == emaliCheckList[0]) {
+
+  document.getElementById('mail_checker').innerHTML = "access guarantee";
+
+} else if (userMail == emaliCheckList[1]) {
+
+  document.getElementById('mail_checker').innerHTML = "access guarantee";
+
+} else if (userMail == emaliCheckList[2]) {
+
+  document.getElementById('mail_checker').innerHTML = "access guarantee";
+
+} else {
+  document.getElementById('mail_checker').innerHTML = "access denied. Per poter accedere devi avere una mail @boolean.";
 }
-
-// frase stampata esempio
-document.getElementById('mail_checker').innerHTML = "access denied";
