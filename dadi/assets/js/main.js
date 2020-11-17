3a3705c51cb666e2ba6533f9b5d95c5f250cbc04
matteo <matteoezio@me.com>
2020-11-17 16:38:17 +0100

    completato gioco dei dadi

--- dadi/assets/js/main.js
+++ dadi/assets/js/main.js
@@ -3,3 +3,18 @@
 // nome utente
 var userName = prompt("Salve, qual è il suo nome?");
 console.log(userName);
+
+
+// random dice number generator
+var playerDice = Math.floor(Math.random() * 5) + 1;
+var compputerDice = Math.floor(Math.random() * 5) + 1;
+console.log(playerDice, compputerDice);
+
+// winner_verifier
+if (playerDice > compputerDice) {
+  document.getElementById('dice_game').innerHTML = "Ottimo " + userName + ", hai vinto";
+} else if (playerDice < compputerDice) {
+  document.getElementById('dice_game').innerHTML = "Spiacente " + userName + ", hai perso";
+} else {
+  document.getElementById('dice_game').innerHTML = "Paregio";
+}
