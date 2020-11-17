7ab04a2cb04d6fe5192764f7b57b6dcf301e3161
matteo <matteoezio@me.com>
2020-11-17 17:15:36 +0100

    struttura base di mail

--- mail/assets/js/main.js
+++ mail/assets/js/main.js
@@ -1,1 +1,13 @@
 // js di mail_checker
+
+// userMail
+var userMail = prompt("Salve, qual è la tua Email?");
+console.log(userMail);
+
+// check email list
+if (userMail = ) {
+
+}
+
+// frase stampata esempio
+document.getElementById('mail_checker').innerHTML = "access denied";
